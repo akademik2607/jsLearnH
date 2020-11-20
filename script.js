308abef4be0250687dfc7b9e1e79ab5b1bc81d34
akademik2607 <akademik2607@mail.ru>
2020-11-20 10:04:11 +0300

    Задание 2 урока

---
+++ script.js
@@ -0,0 +1,11 @@
+let num = 266219;
+num = 266219 + '';
+let result = 1;
+for(let i = 0; i < num.length; ++i){
+    result *= +num[i];
+}
+console.log(result);
+console.log(result ** 3);
+result = result.toString();
+
+console.log(result.slice(0, 2));
