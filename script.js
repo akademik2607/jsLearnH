51231810a37d0ea1ce6f0d97a0ab6317c74092f4
akademik2607 <akademik2607@mail.ru>
2021-01-09 00:41:49 +0300

    Доработал доп защание к 2 уроку

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for(let i = 0; i < num.length; ++i){
     result *= +num[i];
 }
 console.log(result);
-console.log(result ** 3);
+console.log(result **= 3);
 result = result.toString();
 
 console.log(result.slice(0, 2));
